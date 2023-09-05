4bdb879bc521614dbfbc4b59832c34d83f195857
zhukoff.dmi <happy-beard@mail.ru>
2023-09-05 18:09:52 +0300

    Удалил  BodyParser

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const centralError = require('./middlewares/centralError');
@@ -12,8 +11,6 @@ const config = require('./config');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 mongoose.connect(config.connectDb)
