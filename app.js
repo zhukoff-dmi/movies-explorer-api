ee34791ab872fd6ed81dd37f0c372e3146116fc2
zhukoff.dmi <happy-beard@mail.ru>
2023-09-05 14:20:34 +0300

    Изменил контроллер фильмов

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const centralError = require('./middlewares/centralError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const config = require('./config');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors());
